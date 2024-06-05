ef15186ad982b56a1b61374c0c3c3b7117d618e9
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-06-05 15:23:04 -0500

    created an a cities.js for array of cities

---
+++ scripts/cities.js
@@ -0,0 +1,6 @@
+let cities = [
+    { name: "Benbrook, TX", latitude: 32.6732, longitude: -97.4606 },
+    { name: "Denver, CO", latitude: 39.712179, longitude: -104.973050 },
+    { name: "Portland, OR", latitude: 45.520117, longitude: -122.676683 },
+    { name: "Portland, ME", latitude: 43.662093, longitude: -70.260251 }
+ ];
